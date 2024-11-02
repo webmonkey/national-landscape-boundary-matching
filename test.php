287a0455644e23724cf95f11c5fedf03ed728c95
James Cohen <james.cohen@gmail.com>
2024-11-02 18:03:54 +0000

    this works but is pretty slow - particularly building the bounded boxes

--- test.php
+++ test.php
@@ -2,22 +2,44 @@
 
 include_once("phayes-geoPHP-6855624/geoPHP.inc");
 
-$naturalLandscapes = geoPHP::load(file_get_contents('AONBs.geojson'),'json');
+$json = file_get_contents('AONBs.geojson');
+
+$parsed = json_decode($json);
+$naturalLandscapes = array();
+$bboxes = false;
+
+foreach ($parsed->features as $feature) {
+    $name = $feature->properties->NAME;
+    print("Loading $name". PHP_EOL);
+    $geo = geoPHP::load(json_encode($feature),'json');
+    $naturalLandscapes[$name] = $geo;
+
+    if ($bboxes === false) {
+        $bboxes = $geo->envelope();
+    } else {
+        $bboxes = $bboxes->union($geo);
+    }
+}
 
 $points = array(
     'Newlands Corner' => 'POINT(-0.5064713 51.2329694)',
     'Buckingham Palace' => 'POINT(-0.1419329173761422 51.50133722966322)',
 );
 
-print_r($naturalLandscapes);
+foreach ($points as $pointName => $wkt) {
 
-foreach ($points as $name => $wkt) {
-    
+    print("Looking up $pointName". PHP_EOL);
     $point = geoPHP::load($wkt, 'wkt');
 
-    if ($point->intersects($naturalLandscapes)) {
-        print("$name is in an AONB". PHP_EOL);
-    } else {
-        print("$name is not in an AONB". PHP_EOL);
+    if(! $point->intersects($bboxes) ) {
+        print("$pointName is not in an AONB". PHP_EOL);
+        continue;
+    }
+
+    foreach($naturalLandscapes as $naturalLandscapeName => $naturalLandscape) {
+        if ($point->intersects($naturalLandscape)) {
+            print("$pointName is in $naturalLandscapeName". PHP_EOL);
+            break;
+        }
     }
 }
